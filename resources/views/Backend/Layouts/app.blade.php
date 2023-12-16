6d8744da888ae9f33e5a1a0e367dcd2a601d4522
William Salazar <williamyenn@gmail.com>
2023-12-16 18:10:27 -0400

    primeras vistas y layouts

---
+++ resources/views/Backend/Layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        @include('Backend.Layouts.common-head')
+    </head>
+
+    <body class="g-sidenav-show  bg-gray-200">
+
+    @include('Backend.Layouts.sidebar')
+
+        <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+            @include('Backend.Layouts.header')
+
+
+            @yield('main-content')
+            @include('Backend.Layouts.footer')
+
+        </main>
+
+    @include('Backend.Layouts.common-end')
+    @stack('custom-scripts')
+
+    </body>
+
+</html>
